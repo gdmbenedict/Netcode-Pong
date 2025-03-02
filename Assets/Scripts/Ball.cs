d97180c5ac9404f6de97f4649907e6269c4160c2
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-03-01 20:44:22 -0400

    Ball Logic Change
    changed the ball logic to handle its own physics rather than using the unity physics.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,15 +5,16 @@ using Unity.Netcode;
 
 public class Ball : NetworkBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float startSpeed;
     [SerializeField] private float ballRadius;
     [SerializeField][Range(0, 1)] private float speedIncrease;
     [SerializeField] private float playerRadius;
     [SerializeField] private float playerHitDeviation = 1f;
+    [SerializeField] private LayerMask collisionDetectionMask;
 
     private float p1GoalPos; //assumed left player
     private float p2GoalPos; //assumed right player
+    private Vector2 velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +23,10 @@ public class Ball : NetworkBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         CheckCollision();
+        UpdatePosition();
     }
 
     private void Launch()
@@ -32,7 +34,7 @@ public class Ball : NetworkBehaviour
         //determining which direction to launch the ball
         float x = Random.Range(-1f, 1f);
         float y = Random.Range(-1f, 1f);
-        rb.velocity = new Vector2(x,y).normalized * startSpeed;
+        velocity = new Vector2(x,y).normalized * startSpeed;
     }
 
     //Method that checks for collisions since ball could be moving too fast for regular collision detections
@@ -40,17 +42,24 @@ public class Ball : NetworkBehaviour
     {
         //setting up ray
         Vector2 startPos = transform.position;
-        Vector2 direction = rb.velocity.normalized;
-        float checkDistance = rb.velocity.magnitude * Time.fixedDeltaTime + ballRadius;
-        RaycastHit2D hit = Physics2D.Raycast(startPos, direction, checkDistance);
+        Vector2 direction = velocity.normalized;
+        float checkDistance = velocity.magnitude * Time.deltaTime + ballRadius;
+        RaycastHit2D hit = Physics2D.Raycast(startPos, direction, checkDistance, collisionDetectionMask);
+        Debug.DrawLine(startPos, startPos + direction * checkDistance, Color.red);
 
         //checking for collisions
         if (hit)
         {
+            Debug.Log("collision detected");
+
             //determining which bounce mechanism to call
-            if (hit.collider.tag == "Player")
+            if (hit.collider.CompareTag("Player"))
             {
                 PlayerBounce(hit);
+            }
+            else if (hit.collider.CompareTag("Goal"))
+            {
+                //do goal logic
             }
             else
             {
@@ -62,9 +71,14 @@ public class Ball : NetworkBehaviour
     //Method that performs a reflection of the ball given normal reflection conditions
     private void Bounce(RaycastHit2D hitInfo)
     {
+        Debug.Log("Raycast Bounce Called");
+        Debug.Log(velocity);
+
         Vector2 surfaceNormal = hitInfo.normal;
-        Vector2 newDirection = Vector2.Reflect(rb.velocity.normalized, surfaceNormal);
-        rb.velocity = newDirection.normalized * rb.velocity.magnitude;
+        Vector2 newDirection = Vector2.Reflect(velocity.normalized, surfaceNormal);
+        velocity = newDirection.normalized * velocity.magnitude;
+
+        Debug.Log(velocity);
     }
 
     //Method that perform a reflection of the ball using player as randomizations
@@ -72,7 +86,7 @@ public class Ball : NetworkBehaviour
     {
         //getting the regular reflection to use as a base 
         Vector2 surfaceNormal = hitInfo.normal;
-        Vector2 newDirection = Vector2.Reflect(rb.velocity.normalized, surfaceNormal);
+        Vector2 newDirection = Vector2.Reflect(velocity.normalized, surfaceNormal);
 
         //make impact point affect the bounce
         float difference = (hitInfo.point.y - hitInfo.transform.position.y)/playerRadius;
@@ -86,6 +100,12 @@ public class Ball : NetworkBehaviour
         }
 
         //increasing speed each player bounce to make constantly relfectinn the ball harder
-        rb.velocity = newDirection.normalized * rb.velocity.magnitude * (1+speedIncrease);
+        velocity = newDirection.normalized * velocity.magnitude * (1+speedIncrease);
+    }
+
+    //Method that updates the position of the ball
+    private void UpdatePosition()
+    {
+        transform.position += (Vector3)velocity * Time.deltaTime;
     }
 }
